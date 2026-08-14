e5d13346f1851509a4fb04a6e81bbcad60ad5cd1
aldo278 <antanolopeza@gmail.com>
2026-08-14 13:22:24 -0700

    train/test split

--- Scripts/baseline_tfidf.py
+++ Scripts/baseline_tfidf.py
@@ -13,8 +13,24 @@ from sklearn.metrics import (
 # loading dataset
 df = pd.read_csv("Z:/Devin/Research/annotation_dataset_completed.csv")
 
-df = df.dropna(subset=["Passage", "risk_category"])
-print(df.head())
+df = df.dropna(subset=["Passage", "risk_category"]) # here we simply drop all empty values
+
+
+#       Features and labels
+# let the passsage column be x and the risk_category column be y
+X = df["Passage"]
+y = df["risk_category"]
+
+
+#       Train/test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
+# now we get a sample to train and test on
+print(f"Training samples: {len(X_train)}")
+print(f"Test samples: {len(X_test)}")
+
+
+
 
 
 
